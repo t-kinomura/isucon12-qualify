70384cd0cd855fd89c84c778aba127b5a4d9c081
Tetsu Kinomura <tetsukinomura@gmail.com>
2023-10-08 12:38:03 +0900

    fix: カラムを足した状態でdump取る

--- webapp/sql/tenant_data_dump.sql
+++ webapp/sql/tenant_data_dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff3e4bfaee78c3c3e7f7a663dca4048e1979880ea67d409e4a9015676a62265c
-size 437501171
+oid sha256:8db995d2499ea04229b5b6fa5fca48b1eac8bdfeb201e48e170b891c16d8a2c2
+size 437506048
