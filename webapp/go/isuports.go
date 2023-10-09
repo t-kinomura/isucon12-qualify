88471dd2e522e0d3b1ad5063427c7352400c4984
Tetsu Kinomura <tetsukinomura@gmail.com>
2023-10-09 10:34:35 +0900

    perf: joinが遅いのでやめる

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1212,57 +1212,46 @@ func playerHandler(c echo.Context) error {
 		}
 		return fmt.Errorf("error retrievePlayer: %w", err)
 	}
-	// cs := []CompetitionRow{}
-	// if err := adminDB.SelectContext(
-	// 	ctx,
-	// 	&cs,
-	// 	"SELECT * FROM competition WHERE tenant_id = ? ORDER BY created_at ASC",
-	// 	v.tenantID,
-	// ); err != nil && !errors.Is(err, sql.ErrNoRows) {
-	// 	return fmt.Errorf("error Select competition: %w", err)
-	// }
-
-	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	css := []CompetitionScoreRow{}
-	// pss := make([]PlayerScoreRow, 0, len(cs))
+	cs := []CompetitionRow{}
 	if err := adminDB.SelectContext(
 		ctx,
-		&css,
-		"SELECT c.title, ps.score FROM competition c JOIN player_score ps on c.id = ps.competition_id WHERE c.tenant_id = ? and ps.player_id = ? ORDER BY c.created_at ASC",
+		&cs,
+		"SELECT * FROM competition WHERE tenant_id = ? ORDER BY created_at ASC",
 		v.tenantID,
-		p.ID,
 	); err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return fmt.Errorf("error Select competition: %w", err)
 	}
-	// for _, c := range cs {
-	// 	ps := PlayerScoreRow{}
-	// 	if err := adminDB.GetContext(
-	// 		ctx,
-	// 		&ps,
-	// 		// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-	// 		"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1",
-	// 		v.tenantID,
-	// 		c.ID,
-	// 		p.ID,
-	// 	); err != nil {
-	// 		// 行がない = スコアが記録されてない
-	// 		if errors.Is(err, sql.ErrNoRows) {
-	// 			continue
-	// 		}
-	// 		return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
-	// 	}
-	// 	pss = append(pss, ps)
-	// }
 
-	psds := make([]PlayerScoreDetail, 0, len(css))
-	for _, cs := range css {
-		// comp, err := retrieveCompetition(ctx, ps.CompetitionID)
-		// if err != nil {
-		// 	return fmt.Errorf("error retrieveCompetition: %w", err)
-		// }
+	compIDs := make([]string, 0, len(cs))
+	compIDTitleMap := map[string]string{}
+	for _, c := range cs {
+		compIDs = append(compIDs, c.ID)
+		compIDTitleMap[c.ID] = c.Title
+	}
+
+	pss := []PlayerScoreRow{}
+	whereInPlaceholder := strings.Repeat("?,", len(compIDs)-1) + "?"
+	query := fmt.Sprintf("SELECT * FROM player_score WHERE tenant_id = ? AND competition_id IN (%s) AND player_id = ?", whereInPlaceholder)
+	args := make([]interface{}, 0, len(compIDs)+2)
+	args = append(args, v.tenantID)
+	for _, compID := range compIDs {
+		args = append(args, compID)
+	}
+	args = append(args, p.ID)
+	if err := adminDB.GetContext(
+		ctx,
+		&pss,
+		query,
+		args...,
+	); err != nil {
+		return fmt.Errorf("error Select player_scores: %w", err)
+	}
+
+	psds := make([]PlayerScoreDetail, 0, len(pss))
+	for _, ps := range pss {
 		psds = append(psds, PlayerScoreDetail{
-			CompetitionTitle: cs.Title,
-			Score:            cs.Score,
+			CompetitionTitle: compIDTitleMap[ps.CompetitionID],
+			Score:            ps.Score,
 		})
 	}
 
