6cebcb9410451e6a43d14c5b8e4f9a32bd582bb3
Tetsu Kinomura <tetsukinomura@gmail.com>
2023-10-09 12:12:17 +0900

    perf: visit_historyにindexはる

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -1,4 +1,5 @@
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
-UPDATE id_generator SET id=2678400000 WHERE stub='a';
-ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
+CREATE INDEX visit_history_multi_index ON visit_history (tenant_id, competition_id, player_id);
+-- UPDATE id_generator SET id=2678400000 WHERE stub='a';
+-- ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
